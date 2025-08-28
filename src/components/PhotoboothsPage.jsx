a17bea2850f3efd796c8a3830cc0ce790e43529a
Darshan <109759629+DARSHcverse@users.noreply.github.com>
2025-08-28 20:13:05 +1000

    Update PhotoboothsPage.jsx

--- src/components/PhotoboothsPage.jsx
+++ src/components/PhotoboothsPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import '../App.css';
 import photobooth from '../images/boothimg6.webp';
 import glam from '../images/glam.png';

f2a11b66a4ed4b25b4f2b4d3c159c6ffef32d3c8
Darshan <109759629+DARSHcverse@users.noreply.github.com>
2025-11-01 13:12:06 +1100

    error update

--- src/components/Blog/BlogPost.jsx
+++ src/components/Blog/BlogPost.jsx
@@ -5,16 +5,17 @@ import { post as post1 } from "./posts/photobooth-benefits";
 import { post as post2 } from "./posts/wedding-photo-booth-ideas";
 import { post as post3 } from "./posts/corporate-event-trends";
 import { post as post4 } from "./posts/photobooth-cost-australia";
-import {post as post5} from "./posts/why-choose-photo-booth-with-shan.js";
-import {post as post6} from "./posts/photo-booth-trends-melbourne-2025.js";
-import {post as post7} from "./posts/open-vs-enclosed-photo-booth.js";
-import {post as post8} from "./posts/top 10 props.js";
+import { post as post5 } from "./posts/why-choose-photo-booth-with-shan.js";
+import { post as post6 } from "./posts/photo-booth-trends-melbourne-2025.js";
+import { post as post7 } from "./posts/open-vs-enclosed-photo-booth.js";
+import { post as post8 } from "./posts/top 10 props.js";
 import "../../App.css";
 
 const posts = [post1, post2, post3, post4, post5, post6, post7, post8];
 
 const BlogPost = () => {
   const { slug } = useParams();
+
   const post = posts.find((p) => p.slug === slug);
 
   if (!post) {
@@ -25,16 +26,16 @@ const BlogPost = () => {
     );
   }
 
+  // Normalize canonical URL to avoid duplicates (trailing slash)
+  const canonicalUrl = `https://www.photoboothwithshan.com.au/blog/${post.slug.replace(/\/+$/, "")}/`;
+
   return (
     <>
       <Helmet>
         <title>{post.title}</title>
         <meta name="description" content={post.excerpt} />
         <meta name="keywords" content={post.keywords.join(", ")} />
-        <link
-          rel="canonical"
-          href={`https://www.photoboothwithshan.com.au/blog/${post.slug}`}
-        />
+        <link rel="canonical" href={canonicalUrl} />
       </Helmet>
 
       <section className="blog-post-wrapper">
